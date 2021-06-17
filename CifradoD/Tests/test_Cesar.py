25bae0cc55eae909996a5cbda945c4b32bec0894
dante <dantelazzarin@gmail.com>
2021-06-17 12:21:57 -0300

    Agregando UnitTesting cesar

---
+++ CifradoD/Tests/test_Cesar.py
@@ -0,0 +1,30 @@
+from ..Cesar import encriptarCesar,desencriptarCesar
+import unittest
+
+def test_encriptarCesar():
+    c1 = encriptarCesar("bala")
+    assert c1 == "azkz"
+    c2 = encriptarCesar("ba la")
+    assert c2 == "az kz"
+    c3 = encriptarCesar("ba45la")
+    assert c3 == "az45kz"
+    c4 = encriptarCesar("")
+    assert c4 == ""
+    c5 = encriptarCesar(" ")
+    assert c5 == " "
+    c6 = encriptarCesar(123432)
+    assert c6 == ""
+    c7 = encriptarCesar(2,1)
+    assert c7 == ""
+    c8 = encriptarCesar("@")
+    assert c8 == "@"
+
+
+def test_desencriptarCesar():
+    m1 = desencriptarCesar("BGZXZMMD")
+    assert m1 =="chayanne"
+    m2 = desencriptarCesar("BGZX ZMMD")
+    assert m2 == "chay anne"
+    m3 = desencriptarCesar("zm3zmz ")
+    assert m3 == "an3ana"
+    m4 =
